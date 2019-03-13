3b639b568aca9275bd200cb488850e2fa1a484c3
krishnkant tiwari <krishnkant120@gmail.com>
2019-03-13 18:06:22 +0530

    excel

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "excel");
+$output = '';
+if(isset($_POST["import"]))
+
+{
+  //var_dump();exit();
+// $extension = end(explode(".", $_FILES["excel"]["name"]));
+  $fileName=$_FILES["excel"]["name"];
+  $extension = explode(".",$fileName);  //Line 34
+  $count = count($extension);
+
+ //$extension = end(explode(".", $fileName)); // For getting Extension of selected file
+ $allowed_extension = array("xls", "xlsx", "csv"); //allowed extension
+ if (count($extension)) {
+if(in_array($extension[$count-1], $allowed_extension)) //check selected file extension is present in allowed extension array
+ {
+  $file = $_FILES["excel"]["tmp_name"]; // getting temporary source of excel file
+  //var_dump($file);
+  include("PHPExcel/IOFactory.php"); // Add PHPExcel Library in this code
+  $objPHPExcel = PHPExcel_IOFactory::load($file); // create object of PHPExcel library by using load() method and in load method define path of selected file
+
+  $output .= "<label class='text-success'>Data Inserted</label><br /><table class='table table-bordered'>";
+  foreach ($objPHPExcel->getWorksheetIterator() as $worksheet)
+  {
+   $highestRow = $worksheet->getHighestRow();
+   for($row=2; $row<=$highestRow; $row++)
+   {
+    $output .= "<tr>";
+    $name = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(0, $row)->getValue());
+    $email = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(1, $row)->getValue());
+    $query = "INSERT INTO tbl_excel(excel_name, excel_email) VALUES ('".$name."', '".$email."')";
+    mysqli_query($connect, $query);
+    $output .= '<td>'.$name.'</td>';
+    $output .= '<td>'.$email.'</td>';
+    $output .= '</tr>';
+   }
+  } 
+  $output .= '</table>';
+
+ }
+ else
+ {
+  $output = '<label class="text-danger">Invalid File</label>'; //if non excel file then
+ }
+} }
+ 
+?>
+
+<html>
+ <head>
+  <title>Import Excel to Mysql using PHPExcel in PHP</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+  <style>
+  body
+  {
+   margin:0;
+   padding:0;
+   background-color:#f1f1f1;
+  }
+  .box
+  {
+   width:700px;
+   border:1px solid #ccc;
+   background-color:#fff;
+   border-radius:5px;
+   margin-top:100px;
+  }
+  
+  </style>
+ </head>
+ <body>
+  <div class="container box">
+   <h3 align="center">Import Excel to Mysql using PHPExcel in PHP</h3><br />
+   <form method="post" enctype="multipart/form-data">
+    <label>Select Excel File</label>
+    <input type="file" name="excel" />
+    <br />
+    <input type="submit" name="import" class="btn btn-info" value="Import" />
+   </form>
+   <br />
+   <br />
+   <?php
+   echo $output;
+   ?>
+  </div>
+ </body>
+</html>
